612ef842574552dbf6d4348b2f7a5af991c1ce8f
Lime McMile <135271728+emwimcdo@users.noreply.github.com>
2025-06-28 20:54:55 -0400

    Update webLaunch.py

--- webLaunch.py
+++ webLaunch.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 pages = {
     "Pages": [
+        st.Page("home.py", title="Home"),
         st.Page("About.py", title="About This Website"),
-        st.Page("home.py", title="Home")
     ],
 }
     
