1290afda8c45f584ad4d317637888bf319af6646
Lime McMile <135271728+emwimcdo@users.noreply.github.com>
2025-06-28 21:30:46 -0400

    Update webLaunch.py

--- webLaunch.py
+++ webLaunch.py
@@ -67,7 +67,7 @@ if logIn:
     st.session_state.wantToLogIn = True
 if st.session_state.get("wantToLogIn"):
     st.switch_page("pages/login.py")
-    st.page_link("pages/login.py")
+    st.page_link("pages/login.py", label="Login")
 
 pages = {
     "Pages": [
