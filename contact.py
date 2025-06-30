6d8148f9e9d5e79d56146bd3829c1916aebc00c4
Lime McMile <135271728+emwimcdo@users.noreply.github.com>
2025-06-30 13:24:11 -0400

    Update contact.py

--- contact.py
+++ contact.py
@@ -8,5 +8,6 @@ import dropbox
 import requests
 
 st.title("My Contact Info")
+st.write("Contact me about questions, suggestions, and to ask for commissions.")
 st.write("Email me at emwimcdo@gmail.com")
 st.markdown("[Text Me](sms:+12077479313)")
