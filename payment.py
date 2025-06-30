0323e620a88bfa2e5453d53f3fa171ce9cbeda76
Lime McMile <135271728+emwimcdo@users.noreply.github.com>
2025-06-30 14:28:23 -0400

    Update payment.py

--- payment.py
+++ payment.py
@@ -8,8 +8,8 @@ components.html(
     """
     <div style="text-align: right;">
         <iframe src="https://www.buymeacoffee.com/widget/page/LimeMcMile?description=Support%20me%20on%20Buy%20Me%20a%20Coffee!&message=Thank%20you%20for%20supporting%20me!&color=%2340DCA5"
-                width="100%"
-                height="100%"
+                width="150%"
+                height="200%"
                 style="border:none;">
         </iframe>
     </div>
